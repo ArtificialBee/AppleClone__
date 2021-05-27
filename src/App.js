c075aadcc6470163bea07f820d56887df3b8a500
ArtificialBee <edvinsacic@outlook.com>
2021-05-27 02:04:47 +0200

    Added style to first section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Content1 from './components/content1';
 const App=()=> {
   return(<div className="whole_page"> 
     <Navbar/>
-    <Content1 firstText="Edvin Sacic" secondText="Dobro dosli na moj React webpage!" link1="Learn more" link2="Buy"/>
+    <Content1 firstText="iPad Pro" secondText="Supercharged by the Apple M1 chip." link1="Learn more" link2="Buy"/>
   </div>);
 }
 
--- src/components/content1.js
+++ src/components/content1.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import '../styles/content.css';
+import iPad from '../images/iPad-Pro.jpg';
 
 const Content1=(props)=>{
     return(
-        <div>
-            <h2>{props.firstText}</h2>
-            <h3>{props.secondText}</h3>
-            <div>
+        <div className="first_content">
+            <div class="header">
+                <h1>{props.firstText}</h1>
+                <h3>{props.secondText}</h3>
+            </div>
+
+            <div class="links">
                 <a href="#">{props.link1}</a>
                 <a href="#">{props.link2}</a>
             </div>
